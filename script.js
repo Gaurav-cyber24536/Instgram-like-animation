74803898ddfe3c734cd1e052336684a7ca3007d3
Gaurav Chatterjee <119053540+Gaurav-cyber24536@users.noreply.github.com>
2023-10-02 15:51:50 +0530

    added.html

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let box = document.querySelector("#container")
+let icon = document.querySelector("i")
+
+box.addEventListener("dblclick",function(){
+    icon.style.transform = 'translate(-50%,-50%) scale(1)'
+    icon.style.opacity = 0.8
+
+    setTimeout(function(){
+        icon.style.opacity = 0;
+    },1000);
+    setTimeout(function(){
+        icon.style.transform = 'translate(-50%,-50%) scale(0)'
+    },2000);
+})
